a542aa5fe9a5c2d2b900d6c5881ac797814e747f
Richard Manzoli <manzolidev@gmail.com>
2021-02-18 22:55:19 -0300

    init user api + user schema, install nodemon and mongoose

--- fastify-user-api/index.js
+++ fastify-user-api/index.js
@@ -1,7 +1,59 @@
-const app = require('fastify')({ logger: true });
+const fastify = require('fastify');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
+const app = fastify();
+const mongoUrl = 'mongodb://localhost:27017/users';
+
+const User = require('./User');
+
+try {
+  mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.error(err);
+}
+
+app.get('/api', (req, res) => {
   res.send('Hello!');
+});
+
+// Routes
+// Users
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if (!err) {
+      res.send(users);
+    } else {
+      res.send({ error: err });
+    }
+  });
+});
+
+app.get('/api/users/:id', (req, res) => {
+  const userId = req.params.id;
+
+  User.findById(userId, (err, user) => {
+    if (!err) {
+      res.send(user);
+    } else {
+      res.send({ error: err });
+    }
+  });
+});
+
+app.post('/api/users', (req, res) => {
+  const userToCreate = req.body;
+
+  User.create(userToCreate, (err, createdUser) => {
+    if (!err) {
+      res.send(createdUser);
+    } else {
+      res.send({ error: err });
+    }
+  });
 });
 
 app.listen(3000, (err) => {
